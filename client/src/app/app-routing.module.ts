c06b96a7ca47e494593ea7d55f63b38596e28780
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-05-26 21:36:39 -0700

    Add 404 page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
 import { OrbitalServerService } from "./services/orbital/orbital-server.service";
 
-const routes: Routes = [{ path: "", component: LoginComponent }];
+const routes: Routes = [
+	{ path: "", component: LoginComponent },
+	{ path: "**", component: NotFoundComponent },
+];
 
 @NgModule({
 	imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/login/login.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
 
 @NgModule({
-	declarations: [AppComponent, LoginComponent],
+	declarations: [AppComponent, LoginComponent, NotFoundComponent],
 	imports: [AppRoutingModule],
 	bootstrap: [AppComponent],
 })
