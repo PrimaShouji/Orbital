78a25c2e53765a39da1009560a160dd151ee6218
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-05-26 21:26:39 -0700

    Reorganize project

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule, Routes } from "@angular/router";
-import { LoginComponent } from "./login/login.component";
-import { OrbitalServerService } from "./orbital/orbital-server.service";
+import { LoginComponent } from "./components/login/login.component";
+import { OrbitalServerService } from "./services/orbital/orbital-server.service";
 
 const routes: Routes = [{ path: "", component: LoginComponent }];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { LoginComponent } from "./login/login.component";
+import { LoginComponent } from "./components/login/login.component";
 
 @NgModule({
 	declarations: [AppComponent, LoginComponent],
